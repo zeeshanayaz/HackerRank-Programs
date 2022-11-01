29a39d6868c9669a638de38c2129c5ad45be9e6b
zeeshanayaz <zeeshanayaz1@gmail.com>
2022-11-01 17:56:09 +0500

    Question added in UnexpectedDemand.java file

--- src/ProblemSolvingBasic/UnexpectedDemand.java
+++ src/ProblemSolvingBasic/UnexpectedDemand.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/*A widget manufacturer is facing unexpectedly high demand for its new product.
+They would like to satisfy as many customers as possible. Given a number of widgets
+ available and a list of customer orders, what is the maximum number of orders the
+ manufacturer can fulfill in full?*/
+
 public class UnexpectedDemand {
     public static void main(String[] args) {
         System.out.println("Unexpected Demand!");
